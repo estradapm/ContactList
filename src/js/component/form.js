04516d05d3caf024406eb470df568e717646a0f8
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-06-28 10:00:24 +0000

    guardado

--- src/js/component/form.js
+++ src/js/component/form.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Form = () => {
+
+  const [fName, setFname] = useState(""); 
+  const [email, setEmail] = useState(""); 
+  const [phone, setPhone] = useState(""); 
+  const [address, setAddress] = useState(""); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Verificar los valores al enviar el formulario
+    console.log('Full Name:', fName);
+    console.log('Email:', email);
+    console.log('Phone:', phone);
+    console.log('Address:', address);
+  }
+
+  const handleFnameChange = (e) => {
+    setFname(e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  }
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  }
+
   return (
-    <form>
-        <h1 className="d-flex justify-content-center">Add a new contact</h1>
+    <form onSubmit={handleSubmit}>
+      <h1 className="d-flex justify-content-center">Add a new contact</h1>
       <div className="mb-3">
         <label htmlFor="fullName" className="form-label">Full Name</label>
-        <input
+        <input onChange={handleFnameChange}
           type="text"
           className="form-control"
           id="fullName"
@@ -15,7 +46,7 @@ export const Form = () => {
       </div>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email</label>
-        <input
+        <input onChange={handleEmailChange}
           type="email"
           className="form-control"
           id="email"
@@ -24,7 +55,7 @@ export const Form = () => {
       </div>
       <div className="mb-3">
         <label htmlFor="phone" className="form-label">Phone</label>
-        <input
+        <input onChange={handlePhoneChange}
           type="text"
           className="form-control"
           id="phone"
@@ -33,17 +64,17 @@ export const Form = () => {
       </div>
       <div className="mb-3">
         <label htmlFor="address" className="form-label">Address</label>
-        <input
+        <input onChange={handleAddressChange}
           type="text"
           className="form-control"
           id="address"
           placeholder="Enter address"
         />
       </div>
-        <div class="d-grid gap-2">
-            <input type="submit" className="btn btn-primary" value="Save"></input>
-        </div>
-        <p><a class="link-opacity-100" href="#">or get back to contacts</a></p>
+      <div className="d-grid gap-2">
+        <input type="submit" className="btn btn-primary" value="Save" />
+      </div>
+      <p><a className="link-opacity-100" href="#">or get back to contacts</a></p>
     </form>
   );
 };
